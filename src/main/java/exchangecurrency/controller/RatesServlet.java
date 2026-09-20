de086066e15ec229d207247ad1d77ce0d6a7333c
Konstantin06_ <155472727+KoFeVarKaA@users.noreply.github.com>
2026-09-20 12:32:40 +0300

    Update RatesServlet.java

--- src/main/java/exchangecurrency/controller/RatesServlet.java
+++ src/main/java/exchangecurrency/controller/RatesServlet.java
@@ -1,4 +1,47 @@
 package exchangecurrency.controller;
 
-public class RatesServlet {
+import exchangecurrency.dto.request.RequestPatchRateDto;
+import exchangecurrency.dto.response.ResponseRateDto;
+import exchangecurrency.entity.Currency;
+import exchangecurrency.service.RatesService;
+import exchangecurrency.service.validator.CurrencyValidator;
+import exchangecurrency.utils.ResponseMakerUtil;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+
+@WebServlet(urlPatterns = "/exchangeRates",
+        description = "Добавление нового обменного курса")
+public class RatesServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RatesServlet.class);
+    private final RatesService service;
+
+    public RatesServlet (RatesService service) {this.service = service;}
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        String baseCurrencyCode = request.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = request.getParameter("targetCurrencyCode");
+
+        LOGGER.info("doPOST - /exchangeRate; baseCurrencyCode={},targetCurrencyCode{},rate={}",
+                baseCurrencyCode, targetCurrencyCode,
+                request.getParameter("rate"));
+        CurrencyValidator.validateCode(baseCurrencyCode);
+        CurrencyValidator.validateCode(targetCurrencyCode);
+        RequestPatchRateDto requestDto = new RequestPatchRateDto(
+                baseCurrencyCode, targetCurrencyCode,
+                new BigDecimal(request.getParameter("rate"))
+        );
+        ResponseRateDto responseDto = service.postUpdateRate(requestDto, true);
+        LOGGER.debug("doPOST - /exchangeRate - ответ: {}", responseDto);
+        ResponseMakerUtil.sendJson(response, HttpServletResponse.SC_OK, responseDto);
+    }
 }
